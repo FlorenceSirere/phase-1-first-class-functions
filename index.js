d390d3623c095f340969e340264d43efbbd82821
Moringa School <sirerekatimwa@gmail.com>
2022-09-27 05:10:38 -0400

    completed assignment

---
+++ index.js
@@ -0,0 +1,18 @@
+const receivesAFunction = (callbackFunc) => {
+    return callbackFunc () ;
+}
+const returnsANamedFunction = () => {
+    function AFunc() {
+        // Your code here!!
+    }
+ return AFunc;   
+}
+const returnsAnAnonymousFunction = () => {
+    return () => {};
+}
+
+
+    
+
+
+
